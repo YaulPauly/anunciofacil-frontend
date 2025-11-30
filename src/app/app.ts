2cca652ff24c1a3389621de599cea421a09460d2
Axel <dsg4857@gmail.com>
2025-11-30 10:06:17 -0500

    add: Añadir importaciones de componentes y estructuración HTML

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Hero } from "./components/hero/hero";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
