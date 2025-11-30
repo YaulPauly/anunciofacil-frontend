af7c73aa9debf39c592ce4c124335fb691663c84
Axel <dsg4857@gmail.com>
2025-11-30 10:05:06 -0500

    add: Añadir estados de login para el navbar y responsive

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css'],
 })
 export class Navbar {
+  
+  isLoggedIn = signal(true);
+
+  usuario = signal ({
+    nombre: 'John Doe',
+    email: 'john.doe@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=3'
+  })
+
+  toggleMenu = signal(false);
+
+  isResponsive = signal(false);
+  mobileMenuOpen = signal(false);
 
 }
