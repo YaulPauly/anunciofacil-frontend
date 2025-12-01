5798158f398760d992d06de78cc54b3356203a9b
Axel <dsg4857@gmail.com>
2025-12-01 11:36:15 -0500

    fix: Corregir ruta de Homepage

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Homepage } from './features/public/home/homepage/homepage';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Homepage
+    },
+    
+   
+    
+];
